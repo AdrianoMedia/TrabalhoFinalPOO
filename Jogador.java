ae2202962a3f21bc23fb2fffe3f6755c5a9c8206
Adriano Gabriel Girardi <105896577+AdrianoMedia@users.noreply.github.com>
2022-11-22 19:16:35 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,14 +1,15 @@
 import java.time.LocalDate;
 
-public class Jogador extends Servia {
-	private String nome;
-	private String apelido;
-	private int idade;
-	private float altura;
-	private float peso;
-	private LocalDate dataAniver;
-	private String posicao;
-	private String clubAtual;
+public abstract class Jogador extends Servia {
+	private int number;
+	private String name;
+	private String nickname;
+	private int age;
+	private float height;
+	private float weight;
+	private LocalDate birthDate;
+	private String posicion;
+	private String currentClub;
 	
 	public Jogador(String nome, String apelido, int idade, float altura,
 			float peso, LocalDate dataAniver, String posicao, String clubAtual) {
@@ -23,70 +24,80 @@ public class Jogador extends Servia {
 		this.setPosicao(posicao);
 	}
 	
+	public int getNumero() {
+		return number;
+	}
+
+	public void setNumero(int numero) {
+		if (numero > 0) {
+		this.number = numero;
+		}
+	}
+
 	public String getNome() {
-		return nome;
+		return name;
 	}
 	public void setNome(String nome) {
 		if (nome != null && !nome.isBlank()) {
-		this.nome = nome;
+		this.name = nome;
 		} else
 			throw new IllegalArgumentException("Nome inexistente");
 	}
 	public String getApelido() {
-		return apelido;
+		return nickname;
 	}
 	public void setApelido(String apelido) {
 		if (apelido != null && !apelido.isBlank()) {
-		this.apelido = apelido;
+		this.nickname = apelido;
 		}
 	}
 	public int getIdade() {
-		return idade;
+		return age;
 	}
 	public void setIdade(int idade) {
 		if (idade >= 18) {
-		this.idade = idade;
+		this.age = idade;
 		}
 	}
 	public float getAltura() {
-		return altura;
+		return height;
 	}
 	public void setAltura(float altura) {
 		if (altura > 0) {
-		this.altura = altura;
+		this.height = altura;
 		}
 	}
 	public float getPeso() {
-		return peso;
+		return weight;
 	}
 	public void setPeso(float peso) {
 		if (peso > 0) {
-		this.peso = peso;
+		this.weight = peso;
 		}
 	}
 	public LocalDate getDataAniver() {
-		return dataAniver;
+		return birthDate;
 	}
 	public void setDataAniver(LocalDate dataAniver) {
 		if (dataAniver != null) {
-		this.dataAniver = dataAniver;
+		this.birthDate = dataAniver;
 		}
 	}
 	public String getPosicao() {
-		return posicao;
+		return posicion;
 	}
 	public void setPosicao(String posicao) {
 		if (posicao != null && !posicao.isBlank()) {
-		this.posicao = posicao;
+		this.posicion = posicao;
 		} else
 			throw new IllegalArgumentException("Posição inválida, não tente novamente");
 	}
 	public String getClubAtual() {
-		return clubAtual;
+		return currentClub;
 	}
 	public void setClubAtual(String clubAtual) {
 		if (clubAtual != null && !clubAtual.isBlank()) {
-		this.clubAtual = clubAtual;
+		this.currentClub = clubAtual;
 		} else
 			throw new IllegalArgumentException("Insira um Clube válido");
 	}
